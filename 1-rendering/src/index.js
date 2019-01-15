52981ceef3767e698a8f4cf3bf5f14b2620d52b8
Ryan Florence <rpflorence@gmail.com>
2019-01-15 11:06:45 -0500

    fix imports

--- 1-rendering/src/index.js
+++ 1-rendering/src/index.js
@@ -14,6 +14,6 @@
 //   and then you'll need to call it in the event handlers of the form controls
 ////////////////////////////////////////////////////////////////////////////////
 import "./index.css";
-import "./lecture";
+// import "./lecture";
 // import "./solution";
-// import "./exercise";
+import "./exercise";
