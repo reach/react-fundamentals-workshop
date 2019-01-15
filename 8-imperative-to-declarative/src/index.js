5c41426cbb81623ffb52b13cdf08b438c2b83a8b
Ryan Florence <rpflorence@gmail.com>
2019-01-15 11:10:56 -0500

    and more imports

--- 8-imperative-to-declarative/src/index.js
+++ 8-imperative-to-declarative/src/index.js
@@ -8,8 +8,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+// import App from "./lecture";
+import App from "./exercise";
+// import App from "./solution";
+
 // import App from "./lecture.hooks";
-import App from "./exercise.hooks";
+// import App from "./exercise.hooks";
 // import App from "./solution.hooks";
 
 ReactDOM.render(<App />, document.getElementById("root"));
