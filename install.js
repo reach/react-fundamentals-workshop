38c4f955534339eed8fa1206329cb7d6b0af6b2d
Brad Westfall <brad@azpixels.com>
2019-02-19 14:16:27 -0700

    since we have package locks now, the installer gives a warnign and doesnt install if the user has yarn

--- install.js
+++ install.js
@@ -1,26 +1,26 @@
-const shell = require("shelljs");
+const shell = require('shelljs')
 
-const hasYarn = require("has-yarn");
+// const hasYarn = require('has-yarn')
 
 let lessons = [
-  "1-rendering",
-  "2-composition",
-  "3-state",
-  "4-data-flow",
-  "5-forms",
-  "6-apps",
-  "7-testing",
-  "8-imperative-to-declarative",
-  "9-carousel"
-];
+  '1-rendering',
+  '2-composition',
+  '3-state',
+  '4-data-flow',
+  '5-forms',
+  '6-apps',
+  '7-testing',
+  '8-imperative-to-declarative',
+  '9-carousel'
+]
 
 lessons.forEach(dir => {
-  shell.cd(`${dir}`);
-  shell.echo(`\nInstalling Lesson: ${dir}:`);
-  if (hasYarn) {
-    shell.exec(`yarn`);
-  } else {
-    shell.exec(`npm install`);
-  }
-  shell.cd("..");
-});
+  shell.cd(`${dir}`)
+  shell.echo(`\nInstalling Lesson: ${dir}:`)
+  // if (hasYarn) {
+  //   shell.exec(`yarn`)
+  // } else {
+  shell.exec(`npm install`)
+  // }
+  shell.cd('..')
+})
